81314bed98c2d8784465af78479c3de922489078
joaniebrar <jatinderbrar1981@gmail.com>
2025-02-04 09:48:26 -0700

    addition of index

---
+++ TrendEmber-Solution/TrendEmber.Data/Migrations/TrendsDb/20250203231536_AddTradeSetImportedDateIndex.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TrendEmber.Data.Migrations.TrendsDb
+{
+    /// <inheritdoc />
+    public partial class AddTradeSetImportedDateIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_TradeSet_Name_ImportedDate",
+                table: "TradeSets",
+                column: "ImportedDate"
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_TradeSet_Name_ImportedDate",
+                table: "TradeSets"
+                );
+        }
+    }
+}
